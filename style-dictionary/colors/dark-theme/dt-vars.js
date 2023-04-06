e6385d2c8f7795385c171f780021736aeb54ec15
rmolina <rmolina@sura.cl>
2023-04-06 13:33:55 -0400

    nueva estructura de carpetas

---
+++ style-dictionary/colors/dark-theme/dt-vars.js
@@ -0,0 +1,115 @@
+// config.js
+module.exports = {
+    source: [`__ui/tokens/fixed-colors.json`, `__ui/tokens/colors/theme-dark-colors.json`],
+    // If you don't want to call the registerTransform method a bunch of times
+    // you can override the whole transform object directly. This works because
+    // the .extend method copies everything in the config
+    // to itself, allowing you to override things. It's also doing a deep merge
+    // to protect from accidentally overriding nested attributes.
+    transform: {
+      // Now we can use the transform 'myTransform' below
+      myTransform: {
+        type: 'name',
+        transformer: (token) => token.path.join('_').toUpperCase()
+      }
+    },
+    // Same with formats, you can now write them directly to this config
+    // object. The name of the format is the key.
+    format: {
+        myFormat: ({dictionary, platform}) => {
+  
+           
+          // let nombreFiltrado =  token.name.replace(/(-[a-z])\w+/, ''); 
+          //esto lo hice para eliminar las palabras repetidas
+          // Antes: blue-blue-200
+          // despues: blue-200
+  
+          // let mapName = 'dark-theme-colors';
+          
+
+          let colorTokens = dictionary.allTokens.map(token => {
+
+            
+
+            
+            if(token.type === 'color'){
+          return  `--${token.name}: ${token.value};\n`
+        }
+        
+        }).join('');
+
+
+
+        let opacityImage = dictionary.allTokens.map(token => {
+
+            
+
+            
+          if(token.type === 'opacity'){
+        return  `--${token.name}: ${token.value};\n`
+      }
+     
+      
+      }).join('');
+
+
+
+  
+        //   let shadowTokens = dictionary.allTokens.map(token => {
+        //     if(token.type === 'boxShadow'){
+             
+        //       console.log(token);
+        //       let props = []
+        //       Object.entries(token.value).forEach(([key, value]) => {
+                
+        //         props = [...props, `--${token.name}-${key}:${value};\n`]
+        //       });
+   
+
+        //       return  props                    
+                     
+        // }
+        
+        
+        // }).join('');
+
+
+        //ATENCIÓN:
+        //Esto intencionalmente está generando un archivo de variables vacío ya que hay limitaciones técnicas para implementar el dark mode
+        //Si se desea cambiar debe comentarse la linea 81 y descomentarse la linea 83
+
+        let format = ''
+    
+        // let format = `
+        // @media(prefers-color-scheme: dark) {
+        // :root{
+        //   ${colorTokens}                 
+        //   ${opacityImage}
+        // }
+        // }`
+
+let formatSeparator = format.replaceAll(",", ""); 
+
+console.log(formatSeparator);
+
+          return formatSeparator;
+                
+
+  
+        }
+      },
+    platforms: {
+      
+       //scss
+       "scss": {
+        "transformGroup": "scss",
+        // "prefix": "sd", // agregamos un prefijo a todas las cariables (Le agrega un guión también) 
+        "buildPath": "__ui/abstracts/variables/colors/dark-theme/", // la ruta donde irá nuestro archivo de salida 
+        "files": [{
+          "destination": "dt-vars.scss", // nombre de archivo 
+          "format": "myFormat", // formato 
+          "mapName": "font-style"
+        }]
+      }
+    }
+  }
---
+++ style-dictionary/components/components-compositions.js
@@ -0,0 +1,129 @@
+// config.js
+module.exports = {
+    source: [
+    `__ui/tokens/colors/*`, 
+    `__ui/tokens/foundation-tokens.json`, 
+    `__ui/tokens/components/*`,
+    
+  ],
+    // If you don't want to call the registerTransform method a bunch of times
+    // you can override the whole transform object directly. This works because
+    // the .extend method copies everything in the config
+    // to itself, allowing you to override things. It's also doing a deep merge
+    // to protect from accidentally overriding nested attributes.
+    transform: {
+      // Now we can use the transform 'myTransform' below
+      myTransform: {
+        type: 'name',
+        transformer: (token) => token.path.join('_').toUpperCase()
+      }
+    },
+    // Same with formats, you can now write them directly to this config
+    // object. The name of the format is the key.
+    format: {
+        myFormat: ({dictionary, platform}) => {
+          // let nombreFiltrado =  token.name.replace(/(-[a-z])\w+/, ''); 
+          //esto lo hice para eliminar las palabras repetidas
+          // Antes: blue-blue-200
+          // despues: blue-200
+          // let mapName = 'dark-theme-colors';
+          let tokens = dictionary.allTokens.map(token => {
+            if(token.type === 'composition'){ 
+              let props = []
+              Object.entries(token.value).forEach(([key, value], index) => {
+              //  console.log(token);
+                if(value.toString().startsWith("#")){
+                  let newValue = Object.values(token.original.value)[index];
+                  newValue = newValue.replaceAll(/[{|}]/g, '');
+                  newValue = newValue.replaceAll(/\./g, '-');
+                  newValue = newValue.replaceAll(/_ct_|__ct_+/g, '--ct-');
+                  colorVar = `var(${newValue})`
+                  props = [...props, `--${token.name}-${key}:${colorVar};\n`]
+                  // console.log('____________________ '+ newValue)
+                }
+                else{
+                  props = [...props, `--${token.name}-${key}:${value};\n`]
+                }
+              });
+              return  props                                     
+        } 
+        }).join('');
+
+        let mixins = dictionary.allTokens.map(token => {
+          if(token.type === 'composition'){
+                
+            let props = []
+            Object.entries(token.value).forEach(([key, value], index) => {
+      
+            //  console.log(token);
+      
+            if(key == 'boxShadow'){
+              // console.log('????????=====================::::::: '+Object.values(token.original.value)[index]);
+              let newValue = Object.values(token.original.value)[index];
+              newValue = newValue.replaceAll(/[{|}]/g, '');
+              newValue = newValue.replaceAll(/\./g, '-');
+             
+              
+              let shadow = `box-shadow:`;
+              Object.values(value).forEach((shadowValue, shadowIndex) => {
+                   
+                
+                console.log(token.original.value);
+if(shadowValue != 'dropShadow'){
+
+  if(shadowValue.toString().startsWith("#")){
+    let newValue = Object.values(token.original.value)[index];
+    newValue = newValue.replaceAll(/[{|}]/g, '');
+    newValue = newValue.replaceAll(/\./g, '-');
+    newValue = newValue.replaceAll(/_ct_|__ct_+/g, '--ct-');
+    colorVar = `var(${newValue})`
+
+   
+   
+    shadow = [...shadow, ` var(--shadow-color-base)`]
+    // console.log('____________________ '+ newValue)
+  }else{
+             shadow = [...shadow, ` ${shadowValue}`]
+            }      
+            }
+            });
+              props = `.${token.name}{
+                ${shadow}
+              }\n`
+            }          
+            });
+            return  props                           
+      }
+      
+      
+      }).join('');
+        let format = `
+       
+        :root{
+          ${tokens}
+          
+        }
+
+        ${mixins}
+        `
+let formatSeparator = format.replaceAll(",", ""); 
+// console.log(formatSeparator);
+          return formatSeparator;
+
+        }
+      },
+    platforms: {
+      
+       //scss
+       "scss": {
+        "transformGroup": "scss",
+        // "prefix": "sd", // agregamos un prefijo a todas las cariables (Le agrega un guión también) 
+        "buildPath": "__ui//abstracts/variables/components/compositions/", // la ruta donde irá nuestro archivo de salida 
+        "files": [{
+          "destination": "components-compositions.scss", // nombre de archivo 
+          "format": "myFormat", // formato 
+          "mapName": "font-style"
+        }]
+      }
+    }
+  }
